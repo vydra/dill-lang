e8505b02275360300787ed96b2007b9d7d928b9c
Colin Williams <lackita@gmail.com>
2014-10-03 21:51:30 -0400

    readded some files that accidentally got deleted

---
+++ java/junit/src/test/scala/dill/UtilsSpec.scala
@@ -0,0 +1,16 @@
+package dill
+
+import org.scalatest.Matchers
+import org.scalatest.FlatSpec
+import dill.util.Utils
+
+class UtilsSpec extends FlatSpec with Matchers {
+
+  "Parse feature name from Junit class name" should "be" in {
+
+    Utils.featureNameFromClassName("testbed.WithdrawCashFeatureTest") should be ("WithdrawCash")
+  }
+
+  //TODO: dvydra add negative tests
+
+}
