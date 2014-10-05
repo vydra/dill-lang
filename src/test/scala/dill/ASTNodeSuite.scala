e31aa38d6d75b7423439258980558a02f7c1f584
David Vydra <david@vydra.net>
2014-10-05 11:01:41 -0700

    Started on DataTableSuite

--- src/test/scala/dill/ASTNodeSuite.scala
+++ src/test/scala/dill/ASTNodeSuite.scala
@@ -5,9 +5,11 @@ import org.junit._
 import org.scalatest.junit.ShouldMatchersForJUnit
 import org.scalatest.junit.JUnitSuite
 
-class MoneyNodeSuite extends JUnitSuite {
-  @Test def convert_string_to_big_decimal() {
+class ASTNodeSuite extends JUnitSuite {
+  
+  @Test def mondey_node_convert_string_to_big_decimal() {
     val money_node = new MoneyNode("100.00")
     assert(money_node.asJavaBigDecimal() === BigDecimal("100.00").underlying)
   }
+  
 }
---
+++ src/test/scala/dill/DataTableSuite.scala
@@ -0,0 +1,14 @@
+package dill
+
+import org.scalatest.junit.JUnitSuite
+
+class DataTableSuite extends JUnitSuite {
+  /*
+   * Perhaps use example
+   * http://thomassundberg.wordpress.com/2014/06/30/cucumber-data-tables/
+   * 
+   * and use http://cukes.info/api/cucumber/jvm/javadoc/cucumber/api/DataTable.html
+   * 
+   */
+
+}
