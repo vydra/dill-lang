f7bec67adee7b2a47a7d865bd13dae1dc1859774
David Vydra <david@vydra.net>
2014-10-05 19:28:40 -0700

    started on datatable parser

--- src/main/scala/dill/FeatureParser.scala
+++ src/main/scala/dill/FeatureParser.scala
@@ -2,22 +2,35 @@ package dill
 
 import scala.util.parsing.combinator.JavaTokenParsers
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.MutableList
 
 class FeatureParser extends JavaTokenParsers {
 
+  // override val whiteSpace = """\t+""".r
+
   def charSequenceParser = """.+""".r
 
-  def featureParser = literal("Feature:") ~>  charSequenceParser ^^ {
+  def featureParser = literal("Feature:") ~> charSequenceParser ^^ {
     FeatureNode(_)
   }
 
-  def scenarioParser = literal("Scenario:") ~> charSequenceParser  ~ rep(nameValueParser) ^^ {
-    case scanarioName ~ nameValues =>
-    	val scenario = ScenarioNode(scanarioName)
-    	nameValues.foreach { nv =>
-		      scenario.addSymbol(nv.name, nv.value)
-    	}
-    scenario
+  def scenarioParser = literal("Scenario:") ~> charSequenceParser ~ opt(rep(nameValueParser)) ~ opt(rep(dataTableRowParser)) ^^ {
+    case scanarioName ~ nameValues ~ dataTableRows =>
+      val scenario = ScenarioNode(scanarioName)
+      nameValues match {
+        case Some(nameValues) =>
+          nameValues.foreach { nv =>
+            scenario.addSymbol(nv.name, nv.value)
+          }
+        case None =>
+      }
+      dataTableRows match {
+        case Some(dataTableRows) => dataTableRows.foreach(
+          row => scenario.addDataTableRow(row))
+        case None =>
+      }
+
+      scenario
   }
 
   def upToLeftBraceParser = """[^{]+\{""".r
@@ -25,29 +38,47 @@ class FeatureParser extends JavaTokenParsers {
   def rightEqParser = """[^}]+""".r
 
   def nameValueParser = upToLeftBraceParser ~> leftEqParser ~ literal("=") ~ (decimalNumber | moneyParser | rightEqParser) ~ literal("}") ^^ {
-    case left ~ eq ~ right ~ closingBrace  =>
+    case left ~ eq ~ right ~ closingBrace =>
       val name = left //.split("=").head
       val value = right match {
         case MoneyNode(_) => right.asInstanceOf[MoneyNode].asJavaBigDecimal
         case _ => right
       }
-      val res = NameValueNode(name,value)
-    res
+      val res = NameValueNode(name, value)
+      res
   }
 
   def moneyParser = literal("$") ~> """\d+[.]\d+""".r ^^ {
     MoneyNode(_)
   }
 
+  def dataTableCellParser = literal("|") ~> """[^|]""".r ^^ {
+    new DataCellNode(_)
+  }
+
+  def dataTableRowParser = rep(dataTableCellParser) <~ literal("|") ^^ {
+    case cells =>
+      val row = new DataTableRowNode()
+      cells.foreach { cell =>
+        row.addValue(cell.value)
+      }
+      row
+  }
+
+//  def dataTableParser = rep(dataTableRowParser) ^^ {
+//    case row =>
+//      println("row: " + row)
+//  }
+
   def dillParser = featureParser ~ rep(scenarioParser) ^^ {
     case feature ~ scenarios =>
       scenarios.foreach { scenario =>
-    	  feature.add(scenario)
+        feature.add(scenario)
       }
-     feature
+      feature
   }
 
-  def parse(in : String) = {
+  def parse(in: String) = {
     parseAll(dillParser, in)
   }
 
@@ -55,18 +86,18 @@ class FeatureParser extends JavaTokenParsers {
 
 abstract class ASTNode
 
-case class NameValueNode(val name : String, val value : Any) extends ASTNode {
+case class NameValueNode(val name: String, val value: Any) extends ASTNode {
 
 }
 
-case class FeatureNode(val name : String) extends ASTNode {
+case class FeatureNode(val name: String) extends ASTNode {
   val scenariosMap = HashMap[String, ScenarioNode]()
 
-  def findScenario( name : String) = {
+  def findScenario(name: String) = {
     scenariosMap.get(name)
   }
 
-  def add(s : ScenarioNode) = {
+  def add(s: ScenarioNode) = {
     scenariosMap.put(s.name, s)
     this
   }
@@ -74,21 +105,35 @@ case class FeatureNode(val name : String) extends ASTNode {
   override def toString = name + "\nscenarios: " + scenariosMap.values
 }
 
-case class ScenarioNode(val name : String ) extends ASTNode {
+case class ScenarioNode(val name: String) extends ASTNode {
 
   val symbolTable = HashMap[String, Any]()
+  val dataTable = MutableList[DataTableRowNode]()
 
-  def addSymbol(key : String, value : Any) = {
+  def addSymbol(key: String, value: Any) = {
     symbolTable.put(key, value)
   }
 
-  def get(symbolName : String) = {
+  def get(symbolName: String) = {
     symbolTable(symbolName)
+  }
+
+  def addDataTableRow(row: DataTableRowNode) = {
+    dataTable += row
   }
 
 }
 
-case class MoneyNode(val amount : String) extends ASTNode {
+case class DataCellNode(val value: String) extends ASTNode
+
+case class DataTableRowNode() extends ASTNode {
+  val cellValues = MutableList[String]()
+  def addValue(value: String) {
+    cellValues += value
+  }
+}
+
+case class MoneyNode(val amount: String) extends ASTNode {
   def asJavaBigDecimal() = {
     BigDecimal(amount).underlying
   }
